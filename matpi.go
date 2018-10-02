a397fc34391a1f42796230220b83e695ec572b34
Andrea Manzini <ilmanzo@gmail.com>
2018-10-02 08:35:15 -0500

    first color implementation

--- matpi.go
+++ matpi.go
@@ -10,20 +10,32 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
+var (
+    positiveColor = color.RGBA{0, 100, 0, 0xff}
+    negativeColor = color.RGBA{0, 0, 100, 0xff}
+    zeroColor     = color.RGBA{255, 250, 205, 0xff}
+)
+
 // Convert matrix 'gonum.mat.Matrix' to JPEG picture file with filename.
 // Non-zero matrix element is black.
 func Convert(m mat.Matrix, filename string) (err error) {
+
 	r, c := m.Dims()
 
 	img := image.NewRGBA(image.Rect(0, 0, r, c))
 
 	for i := 0; i < r; i++ {
 		for j := 0; j < c; j++ {
-			if math.Abs(m.At(i, j)) == 0.0 {
-				img.Set(i, j, color.RGBA{255, 250, 205, 0xff})
+			p := m.At(i, j)
+			if math.Abs(p) == 0.0 {
+				img.Set(i, j, zeroColor)
 				continue
 			}
-			img.Set(i, j, color.RGBA{0, 100, 0, 0xff})
+			if p > 0 {
+				img.Set(i, j, positiveColor)
+			} else {
+				img.Set(i, j, negativeColor)
+			}
 		}
 	}
 
